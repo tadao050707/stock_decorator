3395935a319485cb205ecd66ccb7d46d4d334275
KW <kw@KWnoMacBook-Pro.local>
2022-01-28 22:03:52 +0900

    teamsにgroupingsのresourcesをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :groupings
-  resources :teams
+  resources :teams do
+    resources :groupings
+  end
   resources :questions do
     resources :answers
   end
