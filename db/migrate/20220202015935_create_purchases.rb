44e5b646fc928bb7b78ae298c778e783ed4cd848
KW <kw@KWnoMacBook-Pro.local>
2022-02-02 11:12:49 +0900

    create_purchasesのマイグレファイルに, null: falseを追記

--- db/migrate/20220202015935_create_purchases.rb
+++ db/migrate/20220202015935_create_purchases.rb
@@ -1,9 +1,9 @@
 class CreatePurchases < ActiveRecord::Migration[6.1]
   def change
     create_table :purchases do |t|
-      t.bigint :code
-      t.date :date_at
-      t.integer :inputter
+      t.bigint :code, null: false
+      t.date :date_at, null: false
+      t.integer :inputter, null: false
       t.integer :total
       t.references :supplier, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
