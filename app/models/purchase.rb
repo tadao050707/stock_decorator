d3b3be126aa185aed622de44d2004d52401a9e6a
KW <kw@KWnoMacBook-Pro.local>
2022-02-02 11:20:32 +0900

    purchase.rbにアソシエーションを追記

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :supplier
   belongs_to :user
+  has_many :purchase_details, dependent: :destroy
+  has_many :purchased_products, through: :purchase_details, source: :product
+  accepts_nested_attributes_for :purchase_details, allow_destroy: true
 end
