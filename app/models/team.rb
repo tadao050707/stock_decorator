027dee721f45dca02f78f5f103470d34a40c6dbb
KW <kw@KWnoMacBook-Pro.local>
2022-02-02 21:45:12 +0900

    team.rbにhas_many :suppliers, through: :teams_suppliersを追記

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   has_many :teams_clients
   has_many :clients, through: :teams_clients
+  has_many :suppliers, through: :teams_suppliers
   belongs_to :user
   has_many :groupings, dependent: :destroy
   has_many :grouping_users,through: :groupings, source: :user
